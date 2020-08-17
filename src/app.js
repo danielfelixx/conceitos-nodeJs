960f1ad2c7074e20a8d6544d00030afb041f47d4
unknown <danielfelix.c@gmail.com>
2020-08-17 19:03:09 -0300

    final commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,71 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+  repositories.push(repository);
+  response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  if(!isUuid(id)){    
+      response.status(400).json({error:'Invalid Id'});
+   }  
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index < 0) {
+    response.status(400).json({ error: "Id not Found" });
+  }
+    const repository = {     
+      id, 
+      title,
+      url,
+      techs,
+      likes :repositories[index].likes
+    };
+    repositories[index] = repository;
+
+    response.status(200).json(repositories[index]);
+  
+ 
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => repository.id === id);
+
+  if (index < 0) {
+    response.status(400).json({ error: "Invalid Id" });
+  } else {
+    repositories.splice(index, 1);
+    response.status(204).json();
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => repository.id === id);
+  if (index < 0) {
+    response.status(400).json({ error: "Invalid Id" });
+  } else {
+    repositories[index].likes++;
+    response.status(200).json(repositories[index]);
+  }
+
 });
 
 module.exports = app;
